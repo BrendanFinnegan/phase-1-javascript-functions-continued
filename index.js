908d61b2ba8fc0d2c320aef701c2d2f39c19094b
BrendanFinnegan <bf289907@gmail.com>
2021-05-17 21:45:47 -0400

    commit tree 4bd20a2a1ddc753e213d106f792778b1ab994fef

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(a = 'roller-skate') {
+    return `This Saturday, I want to ${a}!`
+    }
+
+    function mondayWork(a = 'go to the office') {
+        return `This Monday, I will ${a}.`
+    }
+    function wrapAdjective(b='*') {
+        return function(a = 'special') {
+            return `You are ${b}${a}${b}!`
+        }
+    }
